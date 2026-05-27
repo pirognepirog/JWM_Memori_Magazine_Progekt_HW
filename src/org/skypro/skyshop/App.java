49e1bcfe24717c2c3d078afc4fd3fa5f4a5138e9
pirognepirog <pirognepirog@gmail.com>
2026-05-27 13:09:32 +0300

    initial commit

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,6 @@
+package org.skypro.skyshop;
+
+public class App {
+
+
+}
---
+++ src/org/skypro/skyshop/Product.java
@@ -0,0 +1,31 @@
+package org.skypro.skyshop;
+
+public class Product {
+
+    // Эти значения должны быть немодифицируемыми  (тогда только геттеры)
+    // после создания объекта, но должна быть возможность получить эти значения в других классах.
+    // Создаю поля класса
+
+    private final String nameProduct;
+    private final int priceProduct;
+
+    //создаю конструктор
+    public Product(String nameProduct, int priceProduct){
+        this.nameProduct = nameProduct;
+        this.priceProduct = priceProduct;
+    }
+    // инициализация геттеров для чтения данных класса
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public int getPriceProduct() {
+        return priceProduct;
+    }
+
+    @Override
+    public String toString() {
+        return nameProduct + ":" + priceProduct;
+    }
+}
