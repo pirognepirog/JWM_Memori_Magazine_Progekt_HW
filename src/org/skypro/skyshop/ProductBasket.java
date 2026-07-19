89b8c1bd2582a1d2b658b386380257ed131a3cbe
pirognepirog <pirognepirog@gmail.com>
2026-07-19 17:58:14 +0300

    commit Java Collections Java Collections Framework: Set

--- src/org/skypro/skyshop/ProductBasket.java
+++ src/org/skypro/skyshop/ProductBasket.java
@@ -4,90 +4,52 @@ import java.util.*;
 
 import org.skypro.skyshop.SimpleProduct;
 
+import static java.lang.Double.sum;
+
 public class ProductBasket {
-    // по заданию нужно убрать массив и сделать его листом
-    // private Product[] products;
-    // private List<Product> products;
+
     private Map<String, List<Product>> products;
-    private int count;
-    private int totalPrice;
+   // private int count;
+   // private int totalPrice;
+    private static final String DEFAULT_CATEGORY = "Все товары";
 
-/*
-    public ProductBasket() { // int size - убран, так как List не имеет размера (динамический набор)
-        //this.products = new Product[size];
-        // по заданию нужно убрать массив и сделать его листом
-        // реализация листа в контрукторе
-        this.products = new ArrayList<>();
-        this.count = 0;
-        this.totalPrase = 0;
-    }
-/*
-Вам нужно заменить список, используемый в продуктовой корзине, на Map.
-Ключом будет имя, а значением — список продуктов (поскольку продукты с одним и тем же именем могут быть добавлены несколько раз).
-Тип Map выберите самостоятельно с учетом того, что у нас есть несколько операций получения продуктов по имени.
- */
 
     public ProductBasket() { // int size - убран, так как List не имеет размера (динамический набор)
-        //this.products = new Product[size];
-        // по заданию нужно убрать массив и сделать его листом
-        // реализация листа в контрукторе
         this.products = new HashMap<>();
-        this.count = 0;
-        this.totalPrice = 0;
+       // this.count = 0;
+       //this.totalPrice = 0;
     }
-
+    // метод для добавления по имени и цене
     public void addProduсtInBasket(String name, int prise) {
-        if (products == null) {
-            System.out.println("Корзина не создана, отсутствует продукт!");
-            return;
-        }
-/*      отключено, так как теперь не имеет смысла следить за размерностью листа
-        так как от не имеет размера (динамический набор данных)
-        if (count < products.length) {
-            products[count] = new SimpleProduct(name, prise);  // теперь работает
-            totalPrase += prise;
-            count++;
-        } else {
-            System.out.println("Невозможно добавить продукт, корзина заполнена!");
-        }
- */
+        Product product = new SimpleProduct(name,prise);
+        addProduсtInBasket(product);
     }
 
-    // новый метод для перегрузки метода addProduсtInBasket
+    // новый метод для перегрузки метода addProduсtInBasket (добавление по объекту)
     public void addProduсtInBasket(Product product) {
         if (products == null) {
             System.out.println("Корзина не создана, отсутствует продукт!");
             return;
         }
-/*      отключено, так как теперь не имеет смысла следить за размерностью листа
-        так как от не имеет размера (динамический набор данных)
-        if (count < products.length) {
-            products[count] = product;
-            totalPrase += product.getPriceProduct();
-            count++;
-        } else {
-            System.out.println("Невозможно добавить продукт, корзина заполнена!");
-        }
-
- */
+        // добавление в категорию DEFAULT_CATEGORY, так как работаем с Map, нужен ключ
+        products.computeIfAbsent(DEFAULT_CATEGORY, k -> new ArrayList<>()).add(product);
     }
 
     // Метод подсчёта специальных товаров (FixPriceProduct и DiscountedProduct)
     public int getSpecialProductCount() {
-        int specialCount = 0;
-        for (List<Product> productList : products.values()) { // цикл — по всем спискам товаров
-            for (Product product : productList) { // цикл — по каждому товару внутри списка
-                if (product != null && product.isSpecial()) {
-                    specialCount++;
-                }
-            }
-
-        }
-        return specialCount;
+      return (int) products.values().stream()
+              .flatMap(Collection::stream)
+              .filter(Objects::nonNull)
+              .filter(Product::isSpecial)
+              .count();
     }
 
-    public int totalPrice () {
-        return totalPrice;
+    public int getTotalPrice () {
+        return products.values().stream()
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .mapToInt(Product::getPriceProduct)
+                .sum();
     }
 
     public void printBacket() {
@@ -95,47 +57,39 @@ public class ProductBasket {
             System.out.println("В корзине пусто!");
             return;
         }
-/*
-        for (Product product : products) {
-            System.out.println(product);;
-        }
-*/
-        for (List<Product> productList : products.values()) { // цикл — по всем спискам товаров
-            for (Product product : productList) { // цикл — по каждому товару внутри списка
-                System.out.println(product);
-            }
-        }
-        System.out.println("Итого: " + totalPrice  + " руб.");
-    }
-/*
-    public boolean verificationBacket(String nameProduct) {
-        for (Product product : products) {
-            if (product != null && product.getNameProduct().equals(nameProduct)) {
-                System.out.println("В корзине найдено наименование продукта: " + nameProduct);
-                return true;
-            }
-        }
-        System.out.println("В корзине не найдено наименование продукта: " + nameProduct);
-        return false;
+        // StreamAPI для печати
+        products.values().stream()
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .forEach(System.out::println);
+
+        System.out.println("Итого: " + getTotalPrice()  + " руб.");
     }
 
- */
+    // проверка продукта по имени
     public boolean verificationBacket(String nameProduct) {
-        for (List<Product> productList : products.values()) { // цикл — по всем спискам товаров
-            for (Product product : productList) { // цикл — по каждому товару внутри списка
-                if (product != null && product.getNameProduct().equals(nameProduct)) {
-                    System.out.println("В корзине найдено наименование продукта: " + nameProduct);
-                    return true;
-                }
+            boolean found = products.values().stream()
+                    // .values() - возвращает все значения из Map<String, List<Product>>
+                    // .stream() - преобразует .values() в поток  (stream)
+                            .flatMap(Collection::stream)//  "разворачивает" вложенные коллекции.
+                                                        // Collection::stream — ссылка на метод, который превращает
+                                                        // каждый List<Product> в Stream<Product>.
+                            .filter(Objects::nonNull)
+                            .anyMatch(product -> product.getNameProduct().equals(nameProduct));
+                            // .anyMatch - проверяет, соответствует ли хотя бы один элемент потока условию.
+                            //product -> product.getNameProduct().equals(nameProduct) — лямбда-выражение (предикат):
+                            //Для каждого товара (product) вызывает getNameProduct() и сравнивает с искомым именем (nameProduct).
+            if (found) {
+                System.out.println("В корзине найдено наименование продукта: " + nameProduct);
+            } else {
+                System.out.println("В корзине не найдено наименование продукта: " + nameProduct);
             }
-        }
-        System.out.println("В корзине не найдено наименование продукта: " + nameProduct);
-        return false;
+    return found;
     }
 
     public void clearBacket() {
         products.clear();
-        totalPrice = 0;
+        //totalPrice = 0;
         System.out.println("Очистка корзины завершена!");
     }
 
